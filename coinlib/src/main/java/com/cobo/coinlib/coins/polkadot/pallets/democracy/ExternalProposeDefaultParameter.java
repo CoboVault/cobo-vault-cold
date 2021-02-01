0abb095fc204eae327ee425832badf3f608b56cf
soralit <18829236489@163.com>
2021-02-01 16:14:20 +0800

    add propose related pallet

---
+++ coinlib/src/main/java/com/cobo/coinlib/coins/polkadot/pallets/democracy/ExternalProposeDefaultParameter.java
@@ -0,0 +1,34 @@
+package com.cobo.coinlib.coins.polkadot.pallets.democracy;
+
+import com.cobo.coinlib.coins.polkadot.ScaleCodecReader;
+import com.cobo.coinlib.coins.polkadot.UOS.Network;
+import com.cobo.coinlib.coins.polkadot.pallets.Parameter;
+import com.cobo.coinlib.coins.polkadot.scale.ScaleCodecWriter;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class ExternalProposeDefaultParameter extends Parameter {
+    private byte[] proposalHash;
+    public ExternalProposeDefaultParameter(String name, Network network, int code, ScaleCodecReader scr) {
+        super(name, network, code, scr);
+    }
+
+    @Override
+    protected void write(ScaleCodecWriter scw) throws IOException {
+        scw.writeByteArray(proposalHash);
+    }
+
+    @Override
+    protected void read(ScaleCodecReader scr) {
+        proposalHash = scr.readByteArray(32);
+    }
+
+    @Override
+    protected JSONObject addCallParameter() throws JSONException {
+        return new JSONObject().put("ProposalHash", Hex.toHexString(proposalHash));
+    }
+}
